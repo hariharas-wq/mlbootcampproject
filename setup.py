0ecd1f509563e3c23b46dd4b781ae32d835a985c
hari <harihara.sudhans2019@vitstudent.ac.in>
2023-10-07 20:08:46 +0530

    file updated

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup,find_packages
+from typing import List
+
+HYPEN_E_DOT='-e .'
+
+def get_requirement(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as f:
+        requirements=f.readlines()
+        requirements=[req.replace("\n","")for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+        return requirements
+setup(
+    name="src",
+    version="0.0.1",
+    author="hari",
+    author_email="harirohith123@gmail.com",
+    install_requires=get_requirement("requirements.txt"),
+    packages=find_packages()
+)
